0de773f51eaf8676c5bc939cff3171e7c78f6a2a
Yasmin <yas.carozzi@gmail.com>
2023-04-18 18:14:08 -0300

    blog pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -5,10 +5,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,15 +25,20 @@ public class Postagem {
 	private Long id;
 
 	@NotBlank(message = "O atributo título é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo texto deve conter no mínimo 05 e no máximo 1000 caracteres")
+	@Size(min = 5, max = 100, message = "O atributo texto deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo;
 	
 	@NotBlank(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 100, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 
 	public Long getId() {
 		return id;
@@ -62,8 +70,15 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+		
 	}
 	
-	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}	
 	
 }
